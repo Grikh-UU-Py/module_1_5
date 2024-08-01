1c0098734f9c09cfb2a94ac52011468370486c77
Grikh-UU-Py <5186415@mail.ru>
2024-08-01 11:28:14 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var=(1,5,"7",'world',str)
+print(immutable_var)
+
+# immutable_var[1]=9 # в кортеже нельзя обращаться к элементам кроме встроенного списка
+
+mutable_list=[3,7,9]
+print(mutable_list)
+mutable_list[2]=8
+print(mutable_list)
